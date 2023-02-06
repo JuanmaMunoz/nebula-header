6cb534c998968f4c850a0ed8bb9d1e3f0d99cf37
Jua Manuel Muñoz <juan.munoz@vintegris.com>
2023-02-06 13:57:47 +0100

    logic doc

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,56 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { IDropDown } from 'src/app/models/interfaces';
+import { filter, Subscription } from 'rxjs';
+import { IDropDown, INavigationEnd } from 'src/app/models/interfaces';
 import { assetUrl } from 'src/single-spa/asset-url';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
   public urlLogo: string = assetUrl('images/svg/logo.svg');
   public urlUser: string = assetUrl('images/svg/user.svg');
   public userName: string = 'Juan Carlos Fariña';
   public userCompany: string = 'Vintegris';
   public languageSelected = localStorage.getItem('nebulaLanguage');
+  public backNavigation = false;
   public dropDownList: IDropDown[] = [
     {
       name: 'logOut',
-      label:'dropDown.logOut'
+      label: 'dropDown.logOut',
     },
     {
       name: 'changeLanguage',
-      label:'dropDown.changeLanguage'
-    }
-  ]
+      label: 'dropDown.changeLanguage',
+    },
+  ];
 
-  constructor(private translate: TranslateService){}
+  constructor(private translate: TranslateService, private router: Router) {}
 
-  ngOnInit(): void{}
+  ngOnInit(): void {
+    this.subscription.add(
+      this.router.events.pipe(filter((r) => r instanceof NavigationEnd)).subscribe((data: Partial<INavigationEnd>) => {
+        this.backNavigation = data.url === '/doc' ? true : false;
+      })
+    );
+  }
 
-  public actions($event: string):void{
-    switch($event){
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  public actions($event: string): void {
+    switch ($event) {
       case 'changeLanguage':
         this.changeLanguage();
-      break
+        break;
       case 'logOut':
         this.logOut();
-      break
+        break;
     }
   }
 
-  private changeLanguage (){
+  private changeLanguage() {
     this.languageSelected = this.languageSelected === 'en' ? 'es' : 'en';
     localStorage.setItem('nebulaLanguage', this.languageSelected);
     this.translate.use(this.languageSelected);
-    const evChangeLanguage = new CustomEvent('externalChangeLanguage', {detail:{language: this.languageSelected}});
+    const evChangeLanguage = new CustomEvent('externalChangeLanguage', { detail: { language: this.languageSelected } });
     window.dispatchEvent(evChangeLanguage);
   }
 
-
-
-  private logOut():void{
+  private logOut(): void {
     console.log('logOut*******');
+  }
+
+  public backNebula(): void {
+    const url = sessionStorage.getItem('nebulaLastUrl');
+    if (url) {
+      this.router.navigate([`${url}`]);
+    } else {
+      this.router.navigate(['/users']);
+    }
   }
 }
--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -2,3 +2,10 @@ export interface IDropDown {
   name: string;
   label: string;
 }
+
+export interface INavigationEnd {
+  id: number;
+  url: string;
+  urlAfterRedirects: string;
+  type: number;
+}
